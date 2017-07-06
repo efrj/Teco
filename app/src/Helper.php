393b6a2c5d20c7ed911e972fa661380d0fb34f81
efrj <efrj@hotmail.com>
2017-07-06 15:26:27 -0300

    Refatoração da função short_url

--- app/src/Helper.php
+++ app/src/Helper.php
@@ -1,5 +1,12 @@
 <?php
 
-function short_url() {
-	return 2017;
+function short_url( $url ) {
+    $chars = array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
+
+    if(filter_var($url, FILTER_VALIDATE_URL) === FALSE)
+    {
+        return "Not valid";
+    } else {
+        return 'http://teco.cf/' . $chars[array_rand($chars)] . $chars[array_rand($chars)] . $chars[array_rand($chars)] . $chars[array_rand($chars)] . $chars[array_rand($chars)] . $chars[array_rand($chars)];
+    }
 }
