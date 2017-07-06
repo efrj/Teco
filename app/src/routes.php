0c1d1001410a607c6a790075963c12093c28b668
efrj <efrj@hotmail.com>
2017-07-06 16:45:15 -0300

    Adição da rota de redirecionamento para a url original

--- app/src/routes.php
+++ app/src/routes.php
@@ -3,3 +3,20 @@
 $app->get('/', function ($request, $response) {
     return $this->renderer->render($response, 'index.phtml');
 });
+
+$app->post('/short-url', function ($request, $response) {
+    $url = $request->getParam('url');
+    $short = new \Teco\Model\Url;
+    $short->original_url = $url;
+    $short->short_url = short_url($url);
+    if ( $short->save() )
+    {
+        echo 'http://teco.cf/' . $short->short_url;
+    }
+});
+
+$app->get('/{url}', function ($request, $response) {
+    $url = $request->getAttribute('url');
+    $short = \Teco\Model\Url::where('short_url', $url)->first();
+    return $response->withHeader('Location', $short->original_url);
+});
