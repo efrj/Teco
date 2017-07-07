ba52ee826b5a4fd275566fa51202c1c8086e93eb
efrj <efrj@hotmail.com>
2017-07-07 09:06:16 -0300

    Adição da sidebar ao layout

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -8,8 +8,14 @@ $(function(){
             url: '/short-url',
             data: { url: url },
             success:function(data) {
-                $('.new-url').show('slow');
-                $('#shortUrl').html(data);
+                if ( data == 'Not valid' ) {
+                    $('.new-url').hide();
+                    $('.url-error').show('slow');
+                } else {
+                    $('.url-error').hide();
+                    $('.new-url').show('slow');
+                    $('#shortUrl').html(data);
+                }
             },
             error: function(data) {
                 console.log('data');
