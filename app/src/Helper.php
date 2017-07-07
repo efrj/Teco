72b9e3e4de1d797fdf55b4794c26118acfe16d71
efrj <efrj@hotmail.com>
2017-07-07 01:58:38 -0300

    Refatoração do Helper

--- app/src/Helper.php
+++ app/src/Helper.php
@@ -1,12 +1,29 @@
 <?php
 
-function short_url( $url ) {
+function generate_chars( $quantity_chars ) {
     $chars = array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
+    $chars_increment = '';
+    for ( $i=1; $i<=$quantity_chars; $i++ ) {
+        $chars_increment .= $chars[array_rand($chars)];
+    }
+    return $chars_increment;
+}
 
-    if(filter_var($url, FILTER_VALIDATE_URL) === FALSE)
-    {
+function validate_url( $url ) {
+    $http = 'http';
+    $find_http = strpos($url, $http);
+
+    if ( $find_http === false ) {
+        $url = 'http://' . $url;
+    }
+
+    if( filter_var($url, FILTER_VALIDATE_URL) === FALSE ) {
         return "Not valid";
     } else {
-        return $chars[array_rand($chars)] . $chars[array_rand($chars)] . $chars[array_rand($chars)] . $chars[array_rand($chars)] . $chars[array_rand($chars)] . $chars[array_rand($chars)];
+        return $url;
     }
+}
+
+function short_url( $url ) {
+    return $short_url;
 }
